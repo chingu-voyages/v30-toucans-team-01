baeecccccd3a4dbe33a92e08f435dccf3e4323ac
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-05-12 12:39:41 -0700

    Added Basic React App Files
    Deleted all unnecessary files.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WebcamComponent from './components/Webcam';
+
+
+
+function App() {
+  return (
+    <>
+      <WebcamComponent />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Webcam.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Webcam from 'react-webcam';
+import '../assets/css/style.css';
+
+function WebcamComponent() {
+    return (
+        <>
+            <Webcam className="webcamComponent" />
+        </>
+    )
+}
+
+export default WebcamComponent
