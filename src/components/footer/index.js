e70d0f5e2a28c8b301f4824848358562f3141f99
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-05-21 07:12:08 -0700

    Added CSS File and changed filename

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import githubLogo from '../../images/repo.svg';
+import './Footer.css'
 
 function Footer() {
     return (
-        <div>
+        <div className="Footer">
             <p>Chingu Voyages 30 Toucans Team 01</p>
             <img className="Footer__logo" src={githubLogo}/>
         </div>
