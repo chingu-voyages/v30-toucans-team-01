68804be232a4bc26da82ab1f9a06aa5bbc6d22d4
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-05-21 07:16:55 -0700

    Update src/components/footer/index.js
    Co-authored-by: Lily <lilylaw.dev@gmail.com>

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,7 +7,8 @@ function Footer() {
         <div className="Footer">
            <a href="https://github.com/chingu-voyages/v30-toucans-team-01"> 
                <p>Chingu Voyages 30 Toucans Team 01</p>
-            <img className="Footer__logo" src={githubLogo}/>
+                <img className="Footer__logo" src={githubLogo}/>
+            </a>
         </div>
     )
 }
