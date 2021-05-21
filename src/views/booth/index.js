ac5c14b99d7c2aafdf18fd6b028de2cb2164d466
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-05-21 07:43:47 -0700

    Added SVG to link

--- src/views/booth/index.js
+++ src/views/booth/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import WebcamComponent from '../../components/camera'
 import { Link } from 'react-router-dom';
+import backIcon from '../../images/back-icon.svg'
 
 function Booth({children}) {
     return (
         <div className="Booth">
             <WebcamComponent/>
-            <Link to="/">Back</Link>
+            <Link to="/"><img className="Booth__backBtn" src={backIcon}/></Link>
             {children}
         </div>
     )
