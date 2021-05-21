56ef96f705c2f24cf3077947b2a70825d5197719
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-05-21 07:40:58 -0700

    Added home view

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import Footer from '../../components/footer';
+import Header from '../../components/header';
+import Menu from '../../components/menu';
 import './home.css';
 
 function Home() {
     return (
-        <>
-            TODO: HOME VIEW
-        </>
+        <div className="Home">
+            {/* TODO: HOME VIEW */}
+            <Header/>
+            <Menu/>
+            <div>
+                TODO: GALLERY COMPONENT
+            </div>
+            <Footer/>
+        </div>
     );
 }
 
