901dfdbe23571e2be6399790016f2b93cd96a2a3
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-06-01 21:37:53 -0700

    Update index.js

--- src/views/booth/index.js
+++ src/views/booth/index.js
@@ -5,7 +5,30 @@ import backIcon from '../../images/back-icon.svg'
 import Session  from '../../services/sessions'
 import './booth.css'
 
+<<<<<<< Updated upstream
 function Booth({children, addSession, className, addImage}) {
+=======
+function Booth({children, addSession}) {
+    const [expression, setExpression] = useState('');
+    const tempSession = useRef(new Session());
+
+    useEffect(() => {
+        const session = tempSession.current
+        return () => {
+            {session.snapshots.length !== 0 && addSession(session)}
+        }
+    },[])
+    
+    
+    function addImage(props){
+        // Input session
+        tempSession.current.addSnapshot(props, expression);
+        console.log(tempSession);
+        console.log("addImage Test Passed")
+    }
+
+    
+>>>>>>> Stashed changes
     return (
         <div className={`Booth ${className || ''}`}>
             <Camera addImage={addImage}/>
