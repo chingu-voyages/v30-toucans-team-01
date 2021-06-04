c847e111badc1f453725a1d01ecd8b26a8aeabd5
Lily Law <lilylaw.dev@gmail.com>
2021-06-04 13:27:45 +0100

    Add preview image to Booth

--- src/views/booth/index.js
+++ src/views/booth/index.js
@@ -1,14 +1,26 @@
-import React, {useState, useRef, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Camera from '../../components/camera'
 import { Link } from 'react-router-dom'
 import backIcon from '../../images/back-icon.svg'
-import Session  from '../../services/sessions'
 import './booth.css'
 
-function Booth({children, addSession, className, addImage}) {
+function Booth({children, className, addImage}) {
+    const [imagePreview, setImagePreview] = useState()
+    function handleAddImage(imageUrl) {
+        setImagePreview(imageUrl)
+        addImage(imageUrl)
+    }
+    useEffect(() => {
+        imagePreview && setTimeout(() => {
+            setImagePreview(null)
+        }, 1000)
+    }, [imagePreview])
     return (
         <div className={`Booth ${className || ''}`}>
-            <Camera addImage={addImage}/>
+            {imagePreview && <dialog open className="Booth__image-preview">
+                <img className="image-preview__image" src={imagePreview} alt="Image preview" />
+            </dialog>}
+            <Camera addImage={handleAddImage}/>
             <div className="Booth__overlay">
                 <Link to="/" className="Booth__back-button"><img className="Booth__backBtn" alt="back button" src={backIcon}/></Link>
                 <div className="Booth__children">
