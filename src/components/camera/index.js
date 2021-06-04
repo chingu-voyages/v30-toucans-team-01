732f8675a87894b099c27ba7eaf93c501fce4aad
Lily Law <lilylaw.dev@gmail.com>
2021-06-04 14:38:22 +0100

    Add camera checks

--- src/components/camera/index.js
+++ src/components/camera/index.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import Webcam from 'react-webcam';
 import './camera.css';
-import Modal from '../modal';
 
 const videoConstraints = {
     width: 1280,
@@ -10,28 +9,41 @@ const videoConstraints = {
 }
 function Camera({addImage}) {
     const webcamRef = React.useRef();
-    const [hasCamera, setCamera] = useState(navigator.mediaDevices)
+    const [hasCamera, setCamera] = useState(null)
     let imageURL;
 
     async function  grabImage() {
         imageURL = await webcamRef.current.getScreenshot();
         addImage(imageURL);
-    }
-
-
+      }
     return (
-        <div className="Camera" onClick={grabImage}>
-            <Webcam
-                className="Camera__webcam"
-                audio={false}
-                height={720}
-                ref={webcamRef}
-                screenshotFormat="image/webp"
-                width={1280}
-                videoConstraints={videoConstraints}
-                imageSmoothing={true}
-            /> 
-            {!hasCamera && <Modal/>}
+        <div className="Camera__wrapper">
+            {hasCamera === null && 
+                <dialog open>
+                    Accessing camera...'
+                </dialog>}
+            {hasCamera === false ? 
+                <dialog open>
+                    'No Camera found!'
+                    <button onClick={() => setCamera(null)}>
+                        Check again
+                    </button>
+                </dialog> :
+                <div className="Camera" onClick={grabImage}>
+                    {(hasCamera || hasCamera === null) && <Webcam
+                        className="Camera__webcam"
+                        audio={false}
+                        height={720}
+                        ref={webcamRef}
+                        screenshotFormat="image/webp"
+                        width={1280}
+                        videoConstraints={videoConstraints}
+                        imageSmoothing={true}
+                        onUserMediaError={() => setCamera(false)}
+                        onUserMedia={() => setCamera(true)}
+                    />}
+                </div>
+            }
         </div>
     )
 }
