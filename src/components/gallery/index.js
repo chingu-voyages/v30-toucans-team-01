525d70988de298e4b31996c06454d4f399c0282d
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-06-10 23:50:22 -0700

    Revert "Merge branch 'development' into feature/#64-indexeddb-service"
    This reverts commit cab324c2fb53b49c45cc5b156a54e2ef8de078bf, reversing
    changes made to faed864281d6dba41ff1d46ae0a946623ac8c05e.

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,32 +1,12 @@
-import React, {useState} from 'react'
-import Session from './session'
-import Thumbnail from './thumbnail'
-import backIcon from '../../images/back-icon.svg'
+import React from 'react'
 import './gallery.css'
+import {Session} from './snapshot';
+
 
 function Gallery({sessions}) {
-    const navForward = (toComponent) => setNavStack(navStack => [toComponent, ...navStack])
-    const navBackward = () => setNavStack(navStack => navStack.slice(1))
-    const sessionElements = sessions.map((session) => {
-        const sessionElement = <Session openSnapshot={navForward} key={session.id} snapshots={session.snapshots}/>
-        const openSession = () => navForward(sessionElement)
-        return <Thumbnail onClick={openSession} key={session.id} snapshots={session.snapshots}/>
-    })
-    const [navStack, setNavStack] = useState([sessionElements])
-    
     return (
-        <div className="Gallery">
-            <h1 className="Gallery__heading">Gallery</h1>
-            <div className="Gallery__nav">
-                {navStack.length > 1 &&
-                    <button onClick={navBackward} className="Gallery__backBtn">
-                        <img alt="gallery back" src={backIcon}/>
-                    </button>
-                }
-            </div>
-            <div className="Gallery__content">
-                {navStack[0]}
-            </div>
+        <div className="gallery__container">
+            {sessions.map((session, index) =>  <Session key={session.id} snapshots={session.snapshots}/>)}
         </div>
     )
 }
