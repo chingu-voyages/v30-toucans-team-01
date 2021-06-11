b40f79689c1a5fffc0832ed7db64161154064e54
lily-law <lilylaw.dev@gmail.com>
2021-06-11 15:25:46 +0100

    Remove session state from App - Set up session state in gallery - Add session to db from fiveRandomEmojis view

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -1,19 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import SessionObj from '../../services/sessions'
 import Session from './session'
 import Thumbnail from './thumbnail'
 import backIcon from '../../images/back-icon.svg'
+import db from '../../services/faceitdb.js'
 import './gallery.css'
 
-function Gallery({sessions}) {
+function Gallery() {
+    const [navStack, setNavStack] = useState([])
     const navForward = (toComponent) => setNavStack(navStack => [toComponent, ...navStack])
     const navBackward = () => setNavStack(navStack => navStack.slice(1))
-    const sessionElements = sessions.map((session) => {
-        const sessionElement = <Session openSnapshot={navForward} key={session.id} snapshots={session.snapshots}/>
-        const openSession = () => navForward(sessionElement)
-        return <Thumbnail onClick={openSession} key={session.id} snapshots={session.snapshots}/>
-    })
-    const [navStack, setNavStack] = useState([sessionElements])
     
+
+    useEffect(() => {
+        (async () => {
+            let temp = await db.sessions.toArray()
+            const sessions = temp.map(({id, snapshots}) => new SessionObj(id, snapshots))
+            const sessionElements = sessions.map((session) => {
+                const sessionElement = <Session openSnapshot={navForward} key={session.id} snapshots={session.snapshots}/>
+                const openSession = () => navForward(sessionElement)
+                return <Thumbnail onClick={openSession} key={session.id} snapshots={session.snapshots}/>
+            })
+            setNavStack([sessionElements])
+        })()
+    }, [])
     return (
         <div className="Gallery">
             <h1 className="Gallery__heading">Gallery</h1>
