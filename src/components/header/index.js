7743162951997c233769354522b8e900b401aa6a
lily-law <lilylaw.dev@gmail.com>
2021-06-13 11:32:27 +0100

    Update header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -6,7 +6,8 @@ function Header() {
   return (
     <header className="Header">
       <img className="Header__logo" src={logo} alt="logo" />
-      <p className="Header__tagline">Practice your facial expression mimicry with these fun games!</p>
+      <p className="Header__tagline">Practice your facial expression mimicry with this five random emojis game!</p>
+      <p className="Header__note">All images are stored locally on your device</p>
     </header>
   )
 }
