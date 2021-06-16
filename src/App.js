7abc73935b8e9ee44a776e91bda7ea896f2d94cf
Trek182021 <82979983+Trek182021@users.noreply.github.com>
2021-06-16 00:42:22 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,8 +8,45 @@ import Home from './views/home';
 import "./App.css";
 import Booth from './views/booth';
 import FiveRandomEmojis from './views/fiveRandomEmojis';
+import db from './services/faceitdb'
 
 function App() {
+<<<<<<< Updated upstream
+=======
+  const [sessions, setSessions] = useState([]); 
+  const [quota, setQuota] = useState(0);
+  const [usage, setUsage] = useState(0);
+
+  function addToSessions(props) {
+    // console.log(props)
+    setSessions(session => [...sessions, props])
+    db.sessions.add(props)
+    const temp = showEstimatedQuota();
+    console.log(temp)
+  }
+
+  useEffect(() => {
+    console.log(sessions)
+  }, [sessions]) 
+
+  async function showEstimatedQuota() {
+    if (navigator.storage && navigator.storage.estimate) {
+      const estimation = await navigator.storage.estimate();
+      console.log(`Quota: ${estimation.quota}`);
+      console.log(`Usage: ${estimation.usage}`);
+      return estimation.quota, estimation.usage
+    } else {
+      console.error("StorageManager not found");
+    }
+  }
+
+  useEffect(async () => {
+    let temp = await db.sessions.toArray();
+    setSessions(temp)
+  },[])
+
+
+>>>>>>> Stashed changes
   return (
     <Router>
       <Switch>
